93c7124674b52b51e519e017faea4a9f048d25c7
DavidFQ2212 <kaoszcross12@gmail.com>
2021-05-02 16:38:47 -0500

    se se agrega funciones

--- src/calculadora/CALCULADORA.java
+++ src/calculadora/CALCULADORA.java
@@ -4,8 +4,24 @@ package calculadora;
 
 public class CALCULADORA {
 
-    public static void main(String[] args) {
-       
-    }
-    
+public float  Suma(int numero1 ,int  numero2){
+    float res = numero1 + numero2;
+ return res;   
+}
+public float Resta(float numero1,float numero2){
+     float res = numero1 - numero2;
+ return res;   
+   
+}
+public float Multi(float numero1,float numero2){
+     float res = numero1 * numero2;
+ return res;   
+   
+}
+public float Divicion(float numero1,float numero2){
+     float res = numero1 / numero2;
+ return res;   
+   
+}
+
 }
--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -194,6 +194,9 @@ public class Vista extends javax.swing.JFrame {
 
     
     public static void main(String args[]) {
+        CALCULADORA operacion = new CALCULADORA();
+        
+             
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
